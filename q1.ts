628977a58f74579110bed6dacea7c6a42ba2e060
RatchapornB <ratchaporn_b@cmu.ac.th>
2024-07-02 00:57:14 +0700

    Completed

--- q1.ts
+++ q1.ts
@@ -1,5 +1,13 @@
+import { isThisTypeNode } from "typescript";
+
 function typeChecker(a, b) {
-  /* Your code here */
+  if (typeof a === 'string' && typeof b === 'string'){
+    return ' I LOVE YOU';
+  } else if (typeof a === 'number' && typeof b === 'number'){
+    return a + b;
+  } else {
+    return 'NOT MATCHED';
+  }
 }
 
 //Test cases
@@ -15,3 +23,7 @@ console.log(typeChecker(a2, b2));
 console.log(typeChecker(a3, b3));
 
 module.exports = typeChecker;
+
+
+//รหัสนักศึกษา : 660610790
+//ชื่อ-สกุล : รัชชาพร บัวนุช
